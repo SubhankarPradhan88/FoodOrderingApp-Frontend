3aa82834a43bee39b402078fff71b6c622835925
jeffgtxjava <jeffgtxjava@gmail.com>
2021-04-03 21:53:15 +0530

    added the route for the details-screen

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,22 +1,32 @@
+import { Details } from "@material-ui/icons";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../screens/home/Home";
 
 class Controller extends Component {
-    constructor(props) {
-        super(props);
-        this.baseUrl = "http://localhost:8080/api/";
-    }
-    render() {
-        return (
-            <Router>
-                <div>
-                    {/* Handle routing of respective components */}
-                    <Route exact path="/" render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
-                </div>
-            </Router>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.baseUrl = "http://localhost:8080/api/";
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          {/* Handle routing of respective components */}
+          <Route
+            exact
+            path="/"
+            render={(props) => <Home {...props} baseUrl={this.baseUrl} />}
+          />
+          <Route
+            exact
+            path="/restaurant/:id"
+            render={(props) => <Details {...props} baseUrl={this.baseUrl} />}
+          />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default Controller;
