d3dbce6104ad3a7e70d721889b056d2e88ab18e8
jeffgtxjava <jeffgtxjava@gmail.com>
2021-04-03 21:47:39 +0530

    created the details-screen

---
+++ src/screens/details/Details.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import "./Details.css";
+
+export default class Details extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is from Details page</h3>
+      </div>
+    );
+  }
+}
