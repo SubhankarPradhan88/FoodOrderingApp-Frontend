2fdad4819d6cd11b83bad411c41c0e1296ca73b3
jeffgtxjava <jeffgtxjava@gmail.com>
2021-04-04 02:54:48 +0530

    added header componenet

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
+import Header from "../../common/header/Header";
 
 import "./Details.css";
 
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>This is from Details page</h3>
-      </div>
+      <Header
+        displayItems={{
+          displaySearchBar: false,
+        }}
+      />
     );
   }
 }
